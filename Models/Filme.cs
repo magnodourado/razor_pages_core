54bb0f8986df1a8bdc96efea6dbeb8d34538ae31
Magno Dourado <luiz.ti@live.com>
2020-06-06 20:42:47 -0300

    Primeiro Commit

---
+++ Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class Filme
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DataLancamento { get; set; }
+        public string Genero { get; set; }
+        public decimal Preco { get; set; }
+        public string Avaliacao { get; set; }
+    }
+}
---
+++ Pages/Filmes/Edit.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Filmes
+{
+    public class EditModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public EditModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Filme Filme { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Filme = await _context.Filme.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Filme == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Filme).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FilmeExists(Filme.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool FilmeExists(int id)
+        {
+            return _context.Filme.Any(e => e.ID == id);
+        }
+    }
+}
